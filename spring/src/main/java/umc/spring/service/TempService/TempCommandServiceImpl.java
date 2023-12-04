0d43a182641677754249cfb941d0560be2932a1e
hjh <ghkdwngns@naver.com>
2023-12-04 17:52:46 +0900

    temp용

---
+++ spring/src/main/java/umc/spring/service/TempService/TempCommandServiceImpl.java
@@ -0,0 +1,11 @@
+package umc.spring.service.TempService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import umc.spring.service.TempService.TempCommandService;
+
+@Service
+@RequiredArgsConstructor
+public class TempCommandServiceImpl implements TempCommandService {
+
+}
--- spring/src/main/java/umc/spring/service/TempService/TempQueryServiceImpl.java
+++ spring/src/main/java/umc/spring/service/TempService/TempQueryServiceImpl.java
@@ -1,14 +1,15 @@
-package umc.spring.service;
+package umc.spring.service.TempService;
 
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import umc.spring.apiPayload.code.status.ErrorStatus;
 import umc.spring.apiPayload.exception.handler.TempHandler;
+import umc.spring.service.TempService.TempQueryService;
 
 @Service
 @RequiredArgsConstructor
-public class TempQueryServiceImpl implements TempQueryService{
+public class TempQueryServiceImpl implements TempQueryService {
 
     @Override
     public void CheckFlag(Integer flag) {
