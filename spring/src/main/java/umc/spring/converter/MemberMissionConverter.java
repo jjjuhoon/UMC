d80147b19f36da372a160b7742ceca1253d2bb74
hjh <ghkdwngns@naver.com>
2023-12-04 17:45:56 +0900

    MemberMissionConverter

---
+++ spring/src/main/java/umc/spring/converter/MemberMissionConverter.java
@@ -0,0 +1,26 @@
+package umc.spring.converter;
+
+import net.bytebuddy.dynamic.TypeResolutionStrategy;
+import umc.spring.domain.Review;
+import umc.spring.domain.enums.MissionStatus;
+import umc.spring.domain.mapping.UserMission;
+import umc.spring.web.dto.MemberResponseDTO;
+import umc.spring.web.dto.ReviewRequestDTO;
+
+import java.time.LocalDateTime;
+
+import static umc.spring.domain.enums.MissionStatus.CHALLENGING;
+
+public class MemberMissionConverter {
+
+    public static MemberResponseDTO.ChallengeMissionDTO toChallengeMissionDTO(UserMission userMission){
+        return MemberResponseDTO.ChallengeMissionDTO.builder()
+                .userMissionId(userMission.getId())
+                .createdAt(LocalDateTime.now()).build();
+    }
+
+    public static UserMission toUserMission() {
+        return UserMission.builder()
+                .build();
+    }
+}
